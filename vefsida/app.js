01ff9fcf3cf99f34f2d611c79b4771f08ab531e0
Hákon Logi Bergsson <hakonlogibergsson@Hakons-Air.lan>
2022-03-26 03:23:10 +0000

    Gerði ahref nema með js,og búinn að leika mer mikið með javascript strengim lista og dictionaries

--- vefsida/app.js
+++ vefsida/app.js
@@ -1,19 +1,30 @@
-function fizzbuzz(number){
-    if (number % 3 == 0 && number % 5 == 0){
-        console.log("FizzBuzz")
-    }
-    else if (number % 5 == 0){
-        console.log("Buzz")
-    }
-    else if(number % 3 == 0){
-        console.log("Fizz")
-    }
-}
-
-
 let a = function(){
     alert("babygirl")
 }
 
 
 document.getElementById('sidebar').addEventListener('click',a)
+
+let next_page = function(){
+    location.href = "https://www.youtube.com/watch?v=my6IroYcyEI"
+}
+
+document.getElementById('vid').addEventListener('click',next_page)
+
+const btn = document.getElementById('sjomli')
+
+btn.onclick = () => {
+    alert("Yeaaahboyyy");
+}
+
+function sjonni(ar1,arg2){
+    if (ar1 > arg2){
+        console.log(ar1+" is bigger then "+arg2)
+    }else{
+        console.log(`${arg2} is bigger then ${ar1}`)
+    }
+}
+
+sjonni(4,3)
+
+sjonni(2,6)
